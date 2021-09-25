d72a5d8292c39bce672839f693ec7876bf5ceb9b
jl749 <wjdan7051@gmail.com>
2021-09-25 22:51:38 +0900

    sqldump timezone setting

--- sql백업.bat
+++ sql백업.bat
@@ -1,2 +1,2 @@
 @echo off
-C:\xampp\mysql\bin\mysqldump -u root dragon > C:\Users\user\Downloads\dragon_backup.sql
+C:\xampp\mysql\bin\mysqldump -u root dragon --skip-tz-utc > C:\Users\user\Downloads\dragon_backup.sql
